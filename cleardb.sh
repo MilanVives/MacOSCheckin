e8521409f746d5660cdbe45c99a4fc954945b92c
milan <milan.dima@vives.be>
2025-01-23 18:45:10 +0100

    cleardb script automated

--- cleardb.sh
+++ cleardb.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Setting the environment variable
+export CLEANUP_KEY=your-very-long-secret-key-here
+#Clearing the database
 curl -X POST \
   -H "x-cleanup-key: your-very-long-secret-key-here" \
   https://checkin.vives.live/api/v1/maintenance/d41d8cd98f00b204e9800998ecf8427e/cleanup
